9e84b8fd408055bb1a7c4e46faade759bbb364ce
Jessica-GIRAUD <jessica.giraud1@gmail.com>
2020-11-20 12:37:03 +0100

    modify css info button

--- hackathon/src/components/GodCard.jsx
+++ hackathon/src/components/GodCard.jsx
@@ -13,7 +13,7 @@ class GodCard extends Component {
     super(props);
     this.state = {
       isFavorite: false,
-      data: {Data}
+      data: { Data }
     }
   }
 
@@ -24,41 +24,43 @@ class GodCard extends Component {
 
   render() {
     const { god, add, removeGod } = this.props;
-    const {isFavorite} = this.state;
+    const { isFavorite } = this.state;
     return (
       <div className='general'>
-          <div className='godContainer'>
-            <div className='container'>
-              <img src={god.Photo} alt={god.Name} className='godImage' />
-              <h1 className='name'>{god.Name}</h1>
-            </div>
-            <hr className='hr' />
+        <div className='godContainer'>
+          <div className='container'>
+            <img src={god.Photo} alt={god.Name} className='godImage' />
+            <h1 className='name'>{god.Name}</h1>
+          </div>
+          <hr className='hr' />
 
-            <div className='likeContainer'>
-              <div onClick={this.handleFavorite}> 
-                <img className={isFavorite ? 'like' : 'dislike'} src={Like} alt="like" onClick={() => add(god.Key)}/>
-              </div>
-              <div>
-                <img className='dislike' src={Dislike} alt="dislike" onClick={() => removeGod(god.Key)}/>
-              </div>
+          <div className='likeContainer'>
+            <div onClick={this.handleFavorite}>
+              <img className={isFavorite ? 'like' : 'dislike'} src={Like} alt="like" onClick={() => add(god.Key)} />
             </div>
+            <div>
+              <img className='dislike' src={Dislike} alt="dislike" onClick={() => removeGod(god.Key)} />
+            </div>
+          </div>
 
-            <div className="accordion">
+          <div className="accordion">
+            <div>
               <input type="checkbox" id="god-options" className="toggle" />
-              <label className="title" htmlFor="god-options"> 
+              <label className="title" htmlFor="god-options">
                 <span>&#9432;</span>
               </label>
-              <div className="content">
-                <div>
-                  <p>Age : {god.Age}</p>
-                  <p>Genres : {god.Gender}</p>
-                  <p>Sport : {god.Sports}</p>
-                  <p>Passions : {god.Passion}</p>
-                </div>
+            </div>
+            <div className="content">
+              <div>
+                <p>Age : {god.Age}</p>
+                <p>Genres : {god.Gender}</p>
+                <p>Sport : {god.Sports}</p>
+                <p>Passions : {god.Passion}</p>
               </div>
             </div>
-            
-          </div> 
+          </div>
+
+        </div>
       </div>
     )
   }
@@ -75,4 +77,4 @@ class GodCard extends Component {
      add: PropTypes.func.isRequired,
    }; */
 
-export default GodCard ;
+export default GodCard;
